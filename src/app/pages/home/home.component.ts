2e4b4eda0838291b07c8f33ece587c452de422e3
Hs-lky <salimelk787@gmail.com>
2025-02-14 23:44:58 +0100

    Add trust companies section to home component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 // Components
 import { NavbarComponent } from '../../components/home/navbar/navbar.component';
@@ -7,10 +8,16 @@ import { FeaturesComponent } from '../../components/home/features/features.compo
 import { PricingComponent } from '../../components/home/pricing/pricing.component';
 import { FooterComponent } from '../../components/home/footer/footer.component';
 
+interface TrustCompany {
+  name: string;
+  logo: string;
+}
+
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
+    CommonModule,
     NavbarComponent,
     HeroComponent,
     FeaturesComponent,
@@ -20,4 +27,23 @@ import { FooterComponent } from '../../components/home/footer/footer.component';
   templateUrl: './home.component.html',
   styleUrl: './home.component.sass',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  trustCompanies: TrustCompany[] = [
+    {
+      name: 'Microsoft',
+      logo: '../../../../assets/logos/microsoft.svg'
+    },
+    {
+      name: 'Google',
+      logo: '../../../../assets/logos/google.svg'
+    },
+    {
+      name: 'Amazon',
+      logo: '../../../../assets/logos/amazon.svg'
+    },
+    {
+      name: 'Apple',
+      logo: '../../../../assets/logos/apple.svg'
+    }
+  ];
+}
