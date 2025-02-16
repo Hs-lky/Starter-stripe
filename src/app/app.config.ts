0fbafa1fea8442986cb94a9f095e3505a5af90f0
Hs-lky <salimelk787@gmail.com>
2025-02-16 14:09:01 +0100

    Configure HTTP client with fetch for improved network requests

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,7 @@ import {
   provideRouter,
   withInMemoryScrolling,
 } from '@angular/router';
-
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideStore } from '@ngrx/store';
@@ -23,5 +23,6 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes, scrollingFeature),
     provideClientHydration(),
     provideStore(),
+    provideHttpClient(withFetch()),
   ],
 };
