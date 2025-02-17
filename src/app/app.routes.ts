001deffcb6b8c744a93847d8099892d9c642608c
Hs-lky <salimelk787@gmail.com>
2025-02-17 12:45:35 +0100

    Add subscription routes and guard for dashboard navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
 import { DashboardLayoutComponent } from './components/dashboard/layout/dashboard-layout.component';
 import { UserManagementComponent } from './components/dashboard/users/user-management.component';
-import { SubscriptionManagementComponent } from './components/dashboard/subscriptions/subscription-management.component';
+import { SubscriptionGuard } from './core/guards/subscription.guard';
+import { SubscriptionSuccessComponent } from './components/dashboard/subscriptions/subscription-success.component';
+import { SubscriptionCancelComponent } from './components/dashboard/subscriptions/subscription-cancel.component';
 
 // Routes
 import { HomeComponent } from './pages/home/home.component';
+import { SubscriptionManagementComponent } from './components/dashboard/subscriptions/subscription-management.component';
 
 export const routes: Routes = [
   {
@@ -31,8 +34,27 @@ export const routes: Routes = [
       {
         path: 'subscriptions',
         component: SubscriptionManagementComponent
+      },
+      {
+        path: 'subscription/success',
+        component: SubscriptionSuccessComponent
+      },
+      {
+        path: 'subscription/cancel',
+        component: SubscriptionCancelComponent
       }
     ]
+  },
+  {
+    path: 'dashboard/subscriptions',
+    loadComponent: () => import('./components/dashboard/subscriptions/subscription-management.component')
+      .then(m => m.SubscriptionManagementComponent),
+    canActivate: [SubscriptionGuard]
+  },
+  {
+    path: 'dashboard/plans',
+    loadComponent: () => import('./components/dashboard/subscriptions/plan-selection.component')
+      .then(m => m.PlanSelectionComponent)
   },
   {
     path: '**',
