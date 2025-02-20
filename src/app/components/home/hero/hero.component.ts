2a18e192a87c031aa5e21fe5e673420fb0ed01f0
Hs-lky <salimelk787@gmail.com>
2025-02-20 15:36:24 +0100

    Update hero component with relative asset paths and add image dimension methods

--- src/app/components/home/hero/hero.component.ts
+++ src/app/components/home/hero/hero.component.ts
@@ -20,23 +20,45 @@ export class HeroComponent {
   companies: Company[] = [
     {
       name: 'Nike',
-      src: '../../../../assets/logos/nike.svg',
+      src: '/assets/logos/nike.svg',
     },
     {
       name: 'Stripe',
-      src: '../../../../assets/logos/stripe.svg',
+      src: '/assets/logos/stripe.svg',
     },
     {
       name: 'Disney',
-      src: '../../../../assets/logos/disney.svg',
+      src: '/assets/logos/disney.svg',
     },
     {
       name: 'Coca-Cola',
-      src: '../../../../assets/logos/coca-cola.svg',
+      src: '/assets/logos/coca-cola.svg',
     },
     {
       name: 'Nasa',
-      src: '../../../../assets/logos/nasa.svg',
+      src: '/assets/logos/nasa.svg',
     },
   ];
+
+  getImageWidth(name: string): number {
+    switch (name) {
+      case 'Nike': return 88;
+      case 'Stripe': return 106;
+      case 'Disney': return 107;
+      case 'Coca-Cola': return 147;
+      case 'Nasa': return 103;
+      default: return 120;
+    }
+  }
+
+  getImageHeight(name: string): number {
+    switch (name) {
+      case 'Nike': return 31;
+      case 'Stripe': return 44;
+      case 'Disney': return 44;
+      case 'Coca-Cola': return 48;
+      case 'Nasa': return 29;
+      default: return 55;
+    }
+  }
 }
